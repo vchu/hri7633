fc1d05975d84d10b12451b0e1f70315746961e58
Xinyan Yan <voidpointer@gatech.edu>
2014-12-07 13:59:18 -0500

    Delete extra files in face recog

--- sensor_streams/cob_people_perception/cob_people_detection_msgs/src/cob_people_detection_msgs/msg/__init__.py
+++
@@ -1,7 +0,0 @@
-from ._ColorDepthImage import *
-from ._Mask import *
-from ._ColorDepthImageArray import *
-from ._LabelDist import *
-from ._Detection import *
-from ._DetectionArray import *
-from ._Rect import *
