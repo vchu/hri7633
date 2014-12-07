119a7f97ffb1449e631b83472a65644f7da06dfe
Xinyan Yan <voidpointer@gatech.edu>
2014-12-07 13:52:54 -0500

    Save images after each experiment

---
+++ sensor_streams/cob_people_perception/cob_people_detection_msgs/src/cob_people_detection_msgs/msg/__init__.py
@@ -0,0 +1,7 @@
+from ._ColorDepthImage import *
+from ._Mask import *
+from ._ColorDepthImageArray import *
+from ._LabelDist import *
+from ._Detection import *
+from ._DetectionArray import *
+from ._Rect import *
